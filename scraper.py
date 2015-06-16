9b551bdb06a570f5b530a8320f6ad850f4777f06
Friedrich Lindenberg <friedrich@pudo.org>
2015-06-16 18:36:15 +0200

    refactor scraper to be fully stand-alone

--- scraper.py
+++ scraper.py
@@ -1,15 +1,16 @@
-from pprint import pprint
+import dataset
+# from pprint import pprint
 from collections import defaultdict
 import requests
 
-from util import table, DIMENSIONS
+engine = dataset.connect('sqlite:///data.sqlite')
+table = engine['data']
 
-# 'ausgaben', 'einnahmen'
+DIMENSIONS = ['einzelplan', 'gruppe', 'funktion']
 URL = 'http://www.bundeshaushalt-info.de/rest/%s/soll/%s/%s/%s'
 
 
 def tree_scrape(titles, ctx, year, flow, section, address=''):
-    #address = ''
     url = URL % (year, flow, section, address)
     res = requests.get(url)
     try:
@@ -19,13 +20,13 @@ def tree_scrape(titles, ctx, year, flow, section, address=''):
         return
     level = int(data.get('meta').get('levelCur')) + 1
 
-    #pprint(data.get('meta'))
+    # pprint(data.get('meta'))
 
     for child in data.get('childs'):
         if level == 1:
             print [child.get('l')]
-        if data.get('meta').get('levelCur') == data.get('meta').get('levelMax'):
-            #pprint(ctx)
+        if data.get('meta').get('levelCur') == \
+                data.get('meta').get('levelMax'):
             titles[child.get('a')]['value'] = int(child.get('v'))
             titles[child.get('a')]['flexible'] = child.get('f')
             titles[child.get('a')]['title'] = child.get('l')
@@ -35,22 +36,18 @@ def tree_scrape(titles, ctx, year, flow, section, address=''):
         else:
             ctx['%s_%s_address' % (section, level)] = child.get('a')
             ctx['%s_%s_title' % (section, level)] = child.get('l')
-            #ctx['%s_%s_value' % (section, level)] = child.get('v')
-            tree_scrape(titles, ctx.copy(), year, flow, section, child.get('a'))
-    #return titles
-    #print "\n\nXXXXX\n"
+            tree_scrape(titles, ctx.copy(), year, flow, section,
+                        child.get('a'))
 
 
 def bundeshaushalt_scrape():
-    for year in [2012, 2013]:
+    for year in [2012, 2013, 2014, 2015]:
         titles = defaultdict(dict)
         for flow in ['ausgaben', 'einnahmen']:
             for section in DIMENSIONS:
-                #print [year, flow, section]
-                #titles =
                 ctx = {'year': year, 'flow': flow, 'account': section}
                 tree_scrape(titles, ctx, year, flow, section, '')
-        #pprint(dict(titles))
+        # pprint(dict(titles))
         for row in titles.values():
             table.upsert(row, ['year', 'flow', 'address'])
 
